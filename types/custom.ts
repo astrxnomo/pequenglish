46e881827a68e20a8467e6abd39473da190316b5
astrxnomo <lgiraldoo@unal.edu.co>
2024-10-26 16:58:54 -0500

    add tables relations

--- types/custom.ts
+++ types/custom.ts
@@ -1,9 +1,20 @@
 import { type Database } from './supabase'
 
-export type Task = Database['public']['Tables']['tasks']['Row']
-export type Profile = Database['public']['Tables']['profiles']['Row']
-export type Class = Database['public']['Tables']['classes']['Row']
 export type Role = Database['public']['Tables']['users_role']['Row']
+
+export type Profile = Database['public']['Tables']['profiles']['Row'] & {
+  tasks: Array<Database['public']['Tables']['tasks']['Row']>
+  classes: Array<Database['public']['Tables']['classes']['Row']>
+  users_role: Array<Database['public']['Tables']['users_role']['Row']>
+}
+
+export type Task = Database['public']['Tables']['tasks']['Row'] & {
+  profiles: Database['public']['Tables']['profiles']['Row']
+}
+
+export type Class = Database['public']['Tables']['classes']['Row'] & {
+  profiles: Database['public']['Tables']['profiles']['Row']
+}
 
 export type Message =
   | { success: string }
